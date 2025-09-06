bb9b817828da42c1fd7622cc7cffa2a7eb3a3f31
Rui <ydempo@idaten.clinic>
2025-09-06 16:34:18 -0700

    feat: change logo branding

---
+++ packages/lib/constants/branding.ts
@@ -0,0 +1,11 @@
+export type BrandLogoPaths = {
+  color: string;
+  monoLight: string;
+  monoDark: string;
+};
+
+export const BRAND_LOGO: BrandLogoPaths = {
+  color: '/static/logo.svg',
+  monoLight: '/static/logo-mono-light.svg',
+  monoDark: '/static/logo-mono-dark.svg',
+};
